821cb6aecb63c075fb95fd9afa744e62f6a18404
surbhigautam92 <153414180+surbhigautam92@users.noreply.github.com>
2023-12-10 12:07:00 +0530

    Add files via upload
    test

---
+++ python_task_1.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+
+def generate_car_matrix(df)->pd.DataFrame:
+    """
+    Creates a DataFrame  for id combinations.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame: Matrix generated with 'car' values, 
+                          where 'id_1' and 'id_2' are used as indices and columns respectively.
+    """
+    def color_recommend(value):
+    if value == '-1':
+        color = 'red'
+    elif value == '0':
+        color = 'green'
+    else:
+        return
+    return f'background-color: {color}'
+    return df
+(
+    df.style.applymap(color_negative_red, subset=['car'])
+)
+
+def get_type_count(df)->dict:
+    """
+    Categorizes 'car' values into types and returns a dictionary of counts.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        dict: A dictionary with car types as keys and their counts as values.
+    """
+    # Write your logic here
+    dicts=df.to_dict(orient='car')
+    for i in test_dicts:
+    print(i, test_dicts[i])
+    return dict()
+
+
+def get_bus_indexes(df)->list:
+    """
+    Returns the indexes where the 'bus' values are greater than twice the mean.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        list: List of indexes where 'bus' values exceed twice the mean.
+    """
+    # Write your logic here
+
+    return list()
+
+
+def filter_routes(df)->list:
+    """
+    Filters and returns routes with average 'truck' values greater than 7.
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        list: List of route names with average 'truck' values greater than 7.
+    """
+    # Write your logic here
+    dicts=df.to_dict(orient='truck')
+    meanTruck=dicts.mean>7
+    return list()
+
+
+def multiply_matrix(matrix)->pd.DataFrame:
+    """
+    Multiplies matrix values with custom conditions.
+
+    Args:
+        matrix (pandas.DataFrame)
+
+    Returns:
+        pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
+    """
+    # Write your logic here
+
+    return matrix
+
+
+def time_check(df)->pd.Series:
+    """
+    Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pd.Series: return a boolean series
+    """
+    # Write your logic here
+
+    return pd.Series()
